73d0928810cd88b1034679694dfcfeeb27fffaff
sajolkk <sajolkar56@gmail.com>
2024-09-08 00:43:06 +0600

    model fillable & relationship add

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brand extends Model
 {
     use HasFactory;
+    use HasSlug;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
+    use HasSlug;
     use HasFactory;
+
+
+    protected $fillable = ['name', 'slug'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
