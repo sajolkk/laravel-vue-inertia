2dc7fa43610812161406cdfb109af5d74fadb5dd
sajolkk <sajolkar56@gmail.com>
2024-09-08 00:22:41 +0600

    user layout section done

---
+++ resources/js/Pages/User/Layouts/UserLayouts.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+
+
+</script>
+<template>
+    <!-- header -->
+    <Header />
+    <!-- end -->
+    <!-- main content -->
+    
+    <slot />
+    <!-- end -->
+    <!-- footer -->
+
+    <Footer />
+
+    <!-- end -->
+</template>
